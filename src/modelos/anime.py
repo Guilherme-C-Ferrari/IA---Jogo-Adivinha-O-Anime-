e92c3832dc829a852a4c379f9652878edfe94703
Guilherme Carvalho Ferrari <guicferrari@hotmail.com>
2024-03-08 19:08:29 -0300

    Atualização da camada de modelo

--- src/modelos/anime.py
+++ src/modelos/anime.py
@@ -1,8 +1,8 @@
 class Anime():
 
     _nome : str
-    _codigo : int
+    _codigo : str
 
-    def __init__(self, nome: str, codigo : int):
+    def __init__(self, nome: str, codigo : str):
         self._nome = nome
         self._codigo = codigo
--- src/modelos/pergunta.py
+++ src/modelos/pergunta.py
@@ -1,8 +1,8 @@
 class Pergunta():
 
     _descricao : str
-    _codigo : int
+    _codigo : str
 
-    def __init__(self, descricao: str, codigo : int):
+    def __init__(self, descricao: str, codigo : str):
         self._descricao = descricao
         self._codigo = codigo
