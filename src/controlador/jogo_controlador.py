2f944551263c07c0271ef1bc4e941a7d740e26b3
Guilherme Carvalho Ferrari <guicferrari@hotmail.com>
2024-03-08 16:53:18 -0300

    Alteração na nomenclatura do controlador

---
+++ src/controlador/jogo_controlador.py
@@ -0,0 +1,12 @@
+from persistencia.anime_db import AnimeDB
+from persistencia.pergunta_db import PerguntaDB
+
+class JogoControlador:
+
+    @classmethod
+    def rodar_jogo():
+        pass
+
+    @classmethod
+    def finalizar():
+        pass
